ea1caf9c5ea89d7e6c6abca200874778e6c4b835
Matheus Cavalari <matheuscavbarbosa@hotmail.com>
2026-03-13 23:02:06 -0300

    Initial commit - projeto Spring Boot

---
+++ src/main/java/br/com/matheus/insurance/InsurancePolicyRequestServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheus.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancePolicyRequestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancePolicyRequestServiceApplication.class, args);
+	}
+
+}
